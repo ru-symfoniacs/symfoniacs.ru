df12645aee75b65f031e72c71588ab9c6df660cb
Evgeny Fomin <info@jeka.ru>
2017-11-06 21:58:08 +0300

    upgrade to sf4.0beta, remove Makefile

--- backend-app/config/bundles.php
+++ backend-app/config/bundles.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
-    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
